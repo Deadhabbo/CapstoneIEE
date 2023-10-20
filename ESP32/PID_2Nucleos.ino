a264d6a63274fc708e254875750a1dbb7a272a62
Raiandradeing <119002561+Raiandradeing@users.noreply.github.com>
2023-10-20 17:19:12 -0300

    PID_V1 Arreglado y con N Muestras

--- ESP32/PID_2Nucleos.ino
+++ ESP32/PID_2Nucleos.ino
@@ -12,21 +12,43 @@
 AccelStepper stepper1(motorInterfaceType, stepPinMotor1, dirPinMotor1);
 AccelStepper stepper2(motorInterfaceType, stepPinMotor2, dirPinMotor2);
 
-float x1, y1, x2, y2;
+float x1, y_coord1, x2, y2;
+//float delta_X, delta_Y;
+//float outputX, outputY;
 float error_anteriorX = 0.0, error_anteriorY = 0.0;
 float suma_erroresX1 = 0.0, suma_erroresY1 = 0.0, suma_erroresX2 = 0.0, suma_erroresY2 = 0.0;
 const float kx1=10, kx2=0, ky1=0, ky2=10, kix1=5, kix2=1, kiy1=2, kiy2=5;
+
+const int N = 20; // Ajusta esto al número de muestras que quieras conservar
+float erroresX1[N];
+float erroresY1[N];
+float erroresX2[N];
+float erroresY2[N];
+int indiceX1 = 0;
+int indiceY1 = 0;
+int indiceX2 = 0;
+int indiceY2 = 0;
+
 
 // Pseudocódigo para las funciones de PID
 float PID_X(float setPointX, float currentX, float setPointY, float currentY) {
   
     // Implementación del PID para el eje X
 
-    delta_X=setPointX-currentX;
-    delta_Y=setPointY-currentY;
-    suma_erroresX1+=delta_X;
-    suma_erroresY1+=delta_Y;
-    outputX=delta_X*kx1+delta_Y*ky1+suma_erroresX1*kix1+suma_erroresY1*kiy1;
+    float delta_X=setPointX-currentX;
+    float delta_Y=setPointY-currentY;
+    //suma_erroresX1+=delta_X;
+    suma_erroresX1 = suma_erroresX1 + delta_X - erroresX1[indiceX1];
+    erroresX1[indiceX1] = delta_X;
+    indiceX1 = (indiceX1 + 1) % N;
+
+
+    
+    //suma_erroresY1+=delta_Y;
+    suma_erroresY1 = suma_erroresY1 + delta_Y - erroresY1[indiceY1];
+    erroresY1[indiceY1] = delta_Y;
+    indiceY1 = (indiceY1 + 1) % N;
+    float outputX=delta_X*kx1+delta_Y*ky1+suma_erroresX1*kix1+suma_erroresY1*kiy1;
     
     return outputX;
 }
@@ -34,11 +56,19 @@ float PID_X(float setPointX, float currentX, float setPointY, float currentY) {
 float PID_Y(float setPointY, float currentY, float setPointX, float currentX) {
     // Implementación del PID para el eje Y
 
-    delta_X=setPointX-currentX;
-    delta_Y=setPointY-currentY;
-    suma_erroresX2+=delta_X;
-    suma_erroresY2+=delta_Y;
-    outputY=delta_X*kx2+delta_Y*ky2+suma_erroresX2*kix2+suma_erroresY2*kiy2;
+    float delta_X=setPointX-currentX;
+    float delta_Y=setPointY-currentY;
+    //suma_erroresX2+=delta_X;
+
+    suma_erroresX2 = suma_erroresX2 + delta_X - erroresX2[indiceX2];
+    erroresX2[indiceX2] = delta_X;
+    indiceX2 = (indiceX2 + 1) % N;
+    
+    //suma_erroresY2+=delta_Y;
+    suma_erroresY2 = suma_erroresY2 + delta_Y - erroresY2[indiceY2];
+    erroresY2[indiceY2] = delta_Y;
+    indiceY2 = (indiceY2 + 1) % N;
+    float outputY=delta_X*kx2+delta_Y*ky2+suma_erroresX2*kix2+suma_erroresY2*kiy2;
     return outputY;
 }
 
@@ -60,7 +90,7 @@ bool leerCoordenadas() {
             delay(10);
         }
         Serial.read();
-        y1 = data.toFloat();
+        y_coord1 = data.toFloat();
         data = "";
 
         // Leer x2
@@ -87,7 +117,7 @@ bool leerCoordenadas() {
 void Task_Core0(void *pvParameters) {
     while(1) {
         if (leerCoordenadas()) {
-            float outputX = PID_X(x2, x1, y2, y1);
+            float outputX = PID_X( x2, x1, y2, y_coord1);
             stepper1.move(outputX);  // Función ficticia para aplicar el resultado del PID a lo que estés controlando
         }
         delay(100);  // Puedes ajustar este delay según lo que necesites
@@ -97,7 +127,7 @@ void Task_Core0(void *pvParameters) {
 void Task_Core1(void *pvParameters) {
     while(1) {
         if (leerCoordenadas()) {
-            float outputY = PID_Y(y1, y2, x2, x1);
+            float outputY = PID_Y(y_coord1, y2, x2, x1);
             stepper2.move(outputY);  // Función ficticia para aplicar el resultado del PID a lo que estés controlando
         }
         delay(100);  // Puedes ajustar este delay según lo que necesites
