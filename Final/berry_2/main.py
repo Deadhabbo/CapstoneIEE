7e7639871c97fac215cb29349ca2434c80f62ac0
Deadhabbo <jvillegu@uc.cl>
2023-12-05 20:43:01 -0300

    cambios

--- Final/berry_2/main.py
+++ Final/berry_2/main.py
@@ -1,59 +1,59 @@
 import sys
 import json
-
 from Scripts.serverTCP import ServerTCP
 from Scripts.serverUDP import ServerUDP
 from Scripts.control import Control
-#from Scripts.camara_v2 import Camara
 from Scripts.camara import Camara
 import threading
 
-archivo_parametros = open("parametros.json", encoding="utf-8")#Abrimos archivo
-PARAMETROS = json.load(archivo_parametros) # cargamos
+archivo_parametros = open("parametros.json", encoding="utf-8")
+PARAMETROS = json.load(archivo_parametros)
 
 class HiloPrincipal(threading.Thread):
 
     def __init__(self, tcpport, udpport, host):
         super().__init__(name="Principal")
         self.daemon = False
-        self.udpport  = udpport
-        self.tcpport  = tcpport
+        self.udpport = udpport
+        self.tcpport = tcpport
         self.host = host
 
         self.control = Control()
 
-        self.serverTCP = ServerTCP(tcpport, host, self.control)
-        self.serverUDP = ServerUDP(udpport, host)
-
-        self.camara = Camara(self.serverUDP)
-
-        
-        
     def run(self):
-        
         try:
-            self.serverUDP.start()
-            self.camara.start()
-            while True:
-                pass
+            serverTCP = ServerTCP(self.tcpport, self.host, self.control)
+            serverUDP = ServerUDP(self.udpport, self.host)
+            camara = Camara(serverUDP)
+
+            # Iniciar los hilos
+            serverTCP.start()
+            serverUDP.start()
+            camara.start()
+
+            # Esperar a que los hilos terminen
+            serverTCP.join()
+            serverUDP.join()
+            camara.join()
+
         except KeyboardInterrupt:
             print("Finalizado")
+        finally:
+            # Agregar cualquier código de limpieza aquí
+            pass
 
 
 if __name__ == '__main__':
-
     def hook(type_, value, traceback):
         print(type_)
         print(traceback)
     
     tcp_port = 2020
     udp_port = 9999
-    host = PARAMETROS["host"] # Asignamos ip host
+    host = PARAMETROS["host"]
 
     sys.__excepthook__ = hook
 
     principal = HiloPrincipal(tcp_port, udp_port, host)
-
     principal.start()
-        
-    
+    principal.join()  # Esperar a que HiloPrincipal termine antes de salir
