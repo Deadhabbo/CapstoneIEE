2521fc1ed6f5136b405daa69646618e98c0a4c6e
Deadhabbo <jvillegu@uc.cl>
2023-12-05 21:15:36 -0300

    correcion tcp

--- Final/berry_2/Scripts/serverTCP.py
+++ Final/berry_2/Scripts/serverTCP.py
@@ -89,7 +89,7 @@ class ServerTCP:
         while True:
             while True:
                 try: # intentamos leer mensaje
-                    largo_bytes_mensaje = self.client_socket.recv(4) # los 4 bytes del largo
+                    largo_bytes_mensaje = client_socket.recv(4) # los 4 bytes del largo
                     largo_mensaje = int.from_bytes(
                                     largo_bytes_mensaje, byteorder='little')
                     mensaje = bytearray() #Comenzamos bytearray
@@ -101,7 +101,7 @@ class ServerTCP:
                         break
                 while len(mensaje) < largo_mensaje:
                     read_length = min(4096, largo_mensaje - len(mensaje))
-                    mensaje.extend(self.client_socket.recv(read_length))
+                    mensaje.extend(client_socket.recv(read_length))
                 # cargamos
                 cargado = pickle.loads(mensaje)
                 print("Se recibe/cargado", cargado)
